de3e3f0ca5c347aa4317efd0c5a59c22ba9249fc
Jared Linde <jaredlinde@gmail.com>
2022-02-12 02:40:06 -0500

    testing

--- src/components/BaseLayout.vue
+++ src/components/BaseLayout.vue
@@ -40,13 +40,6 @@
               </div>
             </div>
 
-            <div class="button" style="display:inline-block;position:relative;">
-              <img  style="height:100%;width:120%;"  src="../assets/Button.png"/>
-              <div class="button-text" style="">
-                Did id Work?
-              </div>
-            </div>
-            
           </div>
         </div>
       </div>
