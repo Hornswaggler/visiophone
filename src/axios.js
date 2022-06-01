59664104b81b603657bdc0a311fd8227dcd8e2bd
Jared Linde <jaredlinde@gmail.com>
2022-06-01 01:15:08 -0400

    a;slkdfjas;lkdfj

--- src/axios.js
+++ src/axios.js
@@ -4,7 +4,7 @@ const instance = axios.create({
   baseURL: `${process.env.VUE_APP_BASE_URL}`,
   timeout: 1000,
   headers: {
-    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Origin': 'https://visiophone.wtf',
   }
 });
 
