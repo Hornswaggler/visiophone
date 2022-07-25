735a00127409bc3a2a56f282d6487d9047521300
Jared Linde <jaredlinde@gmail.com>
2022-07-25 14:08:54 -0400

    added linter :| merge surgery

---
+++ src/components/sampleSearch/BootlegListSortIcon.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="flex">
+    <div class="sort-icon ">
+      <font-awesome-icon 
+        icon="fas fa-grip-lines"
+        style="margin-bottom:-0.5em;"
+      />
+      <font-awesome-icon icon="fas fa-grip-lines" />
+    </div>
+
+    <div
+      class="sort-icon"
+      style=""
+    >
+      <font-awesome-icon 
+        icon="fas fa-grip" 
+        style="margin-bottom:-0.666em;"
+      />
+      <font-awesome-icon icon="fas fa-grip" />
+    </div>
+  </div>        
+</template>
+
+<script>
+export default {
+  name:'BootlegListSortIcon'
+}
+</script>
